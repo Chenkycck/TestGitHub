eec5c8f97435da2ec459b9e4a83e199e4ab20aa3
ck.chu <chenkychu@hotmail.com>
2018-04-18 12:19:14 +0800

    Testing Project

---
+++ WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (CKTestingEntities entities = new CKTestingEntities())
+            {
+                
+                return entities.Employees.ToList();
+            }
+            
+        }
+
+
+    }
+}
